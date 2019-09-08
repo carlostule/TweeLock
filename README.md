09c35d74def201c4d9a6938ecd3c3c4e67960934
Carlos  Tule Uscanga <carlostule@MacBook-Air-de-Carlos.local>
2019-09-08 15:07:07 -0500

    Pagina de producción

--- README.md
+++
@@ -1,2 +0,0 @@
-# TweeLock
-Motor de búsqueda para twitter que detecta tweets con semántica violenta
---
+++ precache-manifest.e8380652926fa8d3af23ee0357c34e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac907fb44e8f80d1a451601faa442ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eeb841c91de8ed36f1c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "780461a0922a25822157",
+    "url": "/static/js/2.e9b5e48b.chunk.js"
+  },
+  {
+    "revision": "9eeb841c91de8ed36f1c",
+    "url": "/static/js/main.53fd6e71.chunk.js"
+  },
+  {
+    "revision": "a237aee2bedb9b175b57",
+    "url": "/static/js/runtime~main.ebe33da6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
