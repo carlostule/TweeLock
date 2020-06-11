5bd367e95833842a2169b1cb5e3a1100c5eb0dd7
carlostule <carlos.enrique.tule@gmail.com>
2020-06-11 15:12:26 -0500

    Diseño de interfaz: inicio de cambio y modificación de interfaz

--- src/views/Buscador.js
+++ src/views/Buscador.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import { Row, Col, Button, Modal, Form, Table, Spinner } from 'react-bootstrap'
-import axios from 'axios'
-import TagsInput from 'react-tagsinput'
+import React, { Component } from 'react';
+import { Row, Col, Button, Modal, Form, Table, Spinner } from 'react-bootstrap';
+import axios from 'axios';
+import UserCard from '../views/components/UserCard';
 
-import 'react-tagsinput/react-tagsinput.css'
-import styles from '../css/Buscador.module.css'
+import 'react-tagsinput/react-tagsinput.css';
+import styles from '../css/Buscador.module.css';
 
 
-import tweelock from '../images/Tweelock.svg'
-import analisis from '../images/Analisis.svg'
-const TITULO_MODAL = 'BUSCADOR TWEELOCK'
-const CUERPO_MODAL = 'Necesitas introducir el nombre de un usuario que quieras buscar.'
+import tweelock from '../images/Tweelock.svg';
+import analisis from '../images/Analisis.svg';
+import botonLogo from '../images/LogoBotonDesplegado.svg';
 
-const TITULO_ERROR_MODAL = 'ERROR TWEELOCK'
+const TITULO_MODAL = 'BUSCADOR TWEELOCK';
+const CUERPO_MODAL = 'Necesitas introducir el nombre de un usuario que quieras buscar.';
+const TITULO_ERROR_MODAL = 'ERROR TWEELOCK';
+const TITULO_BUSQUEDA_PREVIA = 'Búsquedas previas';
 
 const filtroLocation = [
     'México',
@@ -39,7 +41,7 @@ const filtroLocation = [
     'coacalco',
     'Ecatepec',
     'ecatepec',
-]
+];
 
 class Buscador extends Component {
     constructor(props) {
@@ -49,13 +51,17 @@ class Buscador extends Component {
             usuario: '',
             modalAviso: false,
             modalError: false,
+            modalBusquedaPrevia: false,
             mensajeError: '',
             twitterUsers: [],
+            twitterUser: {},
             tweets: [],
             vistaAnalisis: false,
             nombreUsuario: '',
             loading: false,
-
+            vistaBusquedaPrevia: true,
+            busquedasPrevias: [],
+            ocultarFooter: false,
         }
     }
 
@@ -67,8 +73,35 @@ class Buscador extends Component {
         this.setState({ modalAviso: false, modalError: false, mensajeError: '' })
     }
 
+    handleRegresar = () => {
+        this.setState({ vistaAnalisis: false, vistaBusquedaPrevia: true, usuario: '', ocultarFooter: false });
+    }
+
+    handleModalBusquedasPrevias = () => {
+        const { modalBusquedaPrevia } = this.state;
+        const url = 'http://localhost:8080/busquedasPrevias';
+
+        axios.get(url).then((res) => {
+            console.log(JSON.stringify(res.data));
+            this.setState({ busquedasPrevias: res.data });
+        }).catch((error) => {
+            this.setState({ modalError: true, mensajeError: error });
+            console.log(error);
+        });
+
+        this.setState({ modalBusquedaPrevia: !modalBusquedaPrevia });
+    }
+
     handleAnalisis = (name, screen_name, count) => {
-        this.setState({ vistaAnalisis: !this.state.vistaAnalisis })
+        const { twitterUsers } = this.state;
+        this.setState({ vistaAnalisis: true, ocultarFooter: true });
+        
+        twitterUsers.forEach((user) => {
+            if (screen_name === user.screen_name) {
+                this.setState({ twitterUser: user });
+            }
+        });
+
         if (!this.state.vistaAnalisis) { // esta en la vista de analisis
             this.setState({ nombreUsuario: name })
             const objetoUsuario = {
@@ -76,7 +109,8 @@ class Buscador extends Component {
                 count: count,
             }
 
-            axios.post('https://tweelock-api.azurewebsites.net/buscarTweets', objetoUsuario)
+            // axios.post('https://tweelock-api.azurewebsites.net/buscarTweets', objetoUsuario)
+            axios.post('http://localhost:8080/buscarTweets', objetoUsuario)
                 .then((res) => {
                     console.log(JSON.stringify(res.data))
                     this.setState({ tweets: res.data })
@@ -130,15 +164,15 @@ class Buscador extends Component {
             username: usuario,
             count: 20,
         }
-        console.log(parametrosJSON)
+        console.log(parametrosJSON);
 
         if (usuario === '') {
             this.setState({ modalAviso: true })
         } else {
-            this.setState({ loading: true })
-            axios.post('https://tweelock-api.azurewebsites.net/buscarUsuarios', parametrosJSON)
+            this.setState({ loading: true, vistaBusquedaPrevia: false });
+            // axios.post('https://tweelock-api.azurewebsites.net/buscarUsuarios', parametrosJSON)
+            axios.post('http://localhost:8080/buscarUsuarios', parametrosJSON)
             .then((res) => {
-                // this.setState({ twitterUsers: res.data, loading: false });
                 const usuarios = res.data
                 const arregloTemporal = []
 
@@ -152,64 +186,74 @@ class Buscador extends Component {
                 // Quitamos valores duplicados en el arreglo
                 let set = new Set(arregloTemporal.map(JSON.stringify))
                 const arregloSinDuplicaciones = Array.from(set).map(JSON.parse)
-
                 this.setState({ twitterUsers: arregloSinDuplicaciones, loading: false });
-
-                console.log(JSON.stringify(this.state.twitterUsers));
-
             }).catch((error) => {
-                // this.setState({ modalError: true, mensajeError: error })
-                console.log(error)
-            })
+                this.setState({ modalError: true, mensajeError: error });
+                console.log(error);
+            });
         }
     }
 
     analyticsView = () => {
-        const { tweets, nombreUsuario } = this.state
+        const {
+            tweets,
+            nombreUsuario,
+            twitterUser,
+        } = this.state
         return (
             <div className={styles.contenedorAnalisis}>
                 <Row className={styles.buttonRegresar}>
-                    <Col sm={1} className={styles.columnaBotonRegresar}>
-                     <Button variant="primary" onClick={this.handleAnalisis}>Regresar</Button>
-                    </Col>
                     <Col sm={9} className={styles.columnaTitulo}>
-                      <p className={styles.tituloAnalisis}>Análisis del usuario {nombreUsuario}</p>
+                      <p className={styles.tituloAnalisis}>Análisis de violencia</p>
+                    </Col>
+                    <Col sm={1} className={styles.columnaBotonRegresar}>
+                     <Button variant="primary" onClick={this.handleRegresar}>Regresar</Button>
                     </Col>
                     <Col sm={2} className={styles.columnaBotonRegresar}>
                      <Button variant="danger">Guardar en PDF</Button>
                     </Col>
                 </Row>
+                <div className={styles.contenedorTarjetaUsuario}>
+                    <UserCard
+                        name={twitterUser.name}
+                        nickname={twitterUser.screen_name}
+                        location={twitterUser.location}
+                        followers={twitterUser.followers_count}
+                        startDate={twitterUser.created_at}
+                    />
+                </div>
                 <Row className={styles.columnaAnalisis}>
-                    <img src={analisis} className={styles.imagenAnalisis} alt="prototipo de analisis"/>
-                </Row>
-                <Row className={styles.tablaAnalisis}>
-                    <Table striped bordered hover variant="dark">
-                        <thead>
-                            <tr>
-                                <td>Tweet id</td>
-                                <td>Usuario</td>
-                                <td>Texto</td>
-                                <td>Clasificación</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {(tweets !== null && tweets.length === 0) ? null :
-                            tweets.map((tweet) => {
-                                if (tweet === null) {
-                                    return null;
-                                } 
-                                return(
+                    <Col className={styles.columnaTweets}>
+                        <Row className={styles.tablaAnalisis}>
+                            <Table striped bordered hover variant="dark">
+                                <thead>
                                     <tr>
-                                        <td>{tweet.tweetId}</td>
-                                        <td>{tweet.user}</td>
-                                        <td>{tweet.msg}</td>
-                                        <td>{tweet.classification.tag_name}</td>
+                                        <td>Tweet id</td>
+                                        <td>Texto</td>
+                                        <td>Clasificación</td>
                                     </tr>
-                                )
-                            })
-                        }
-                        </tbody>
-                    </Table>
+                                </thead>
+                                <tbody>
+                                {(tweets !== null && tweets.length === 0) ? null :
+                                    tweets.map((tweet) => {
+                                        if (tweet === null) {
+                                            return null;
+                                        } 
+                                        return(
+                                            <tr>
+                                                <td>{tweet.tweetId}</td>
+                                                <td>{tweet.msg}</td>
+                                                <td>{tweet.classification.tag_name}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                                </tbody>
+                            </Table>
+                        </Row>
+                    </Col>
+                    <Col className={styles.columnaGrafica}>
+                    </Col>
                 </Row>
             </div>
         )
@@ -248,16 +292,36 @@ class Buscador extends Component {
         )
     }
 
+    previousSearchView = () => {
+        return (
+            <Col className={styles.containerPS}>
+                <button className={styles.botonModalPS} onClick={this.handleModalBusquedasPrevias}>
+                    <img src={botonLogo} className={styles.botonLogo} alt="Búsquedas previas"/> 
+                </button>
+            </Col>
+        );
+    }
+
     render() {
-        const { usuario, modalAviso, modalError, mensajeError, vistaAnalisis, loading } = this.state
+        const {
+            usuario,
+            modalAviso,
+            modalError,
+            mensajeError,
+            vistaAnalisis,
+            loading,
+            vistaBusquedaPrevia,
+            modalBusquedaPrevia,
+            busquedasPrevias,
+            ocultarFooter,
+        } = this.state;
         return(
             <div className={styles.container}>
                 <Row className={styles.header}> 
                     <Col className={styles.columnaLogo}>
-                        <img src={tweelock} className={styles.imgLogo}/>
+                        <img src={tweelock} className={styles.imgLogo} alt="Tweelock" />
                     </Col>
                     <Col className={styles.columnaBuscador}>
-                        {/* <TagsInput value={usuario} onChange={this.handleChange} maxTags={1} inputProps={{ className: 'react-tagsinput-input', placeholder: 'Buscar usuario' }}/> */}
                         <Form.Control type="text" placeholder="Buscar usuario" value={usuario} onChange={this.handleChange} />
                     </Col>
                     <Col className={styles.columnaBoton}>
@@ -268,9 +332,20 @@ class Buscador extends Component {
                 </Row>
                 <Row className={styles.body}>
                     {
-                        vistaAnalisis ? this.analyticsView() : this.usersTable()
+                        vistaBusquedaPrevia ?
+                            this.previousSearchView() :
+                               ( vistaAnalisis ? this.analyticsView() : this.usersTable())
                     }
                 </Row>
+                {
+                    !ocultarFooter ? (
+                        <Row className={styles.footer}>
+                            <div className={styles.columnaFooter}>
+                                <p className={styles.infoFooter}>Trabajo Terminal 2019-A038 • Diana Guadalupe Maldonado Ledo • Carlos Enrique Tule Uscanga</p>
+                            </div>
+                        </Row>
+                    ) : null
+                }
                 <Modal show={modalAviso} onHide={this.handleClose}>
                     <Modal.Header closeButton>
                     <Modal.Title>{TITULO_MODAL}</Modal.Title>
@@ -285,6 +360,39 @@ class Buscador extends Component {
                     </Modal.Header>
                     <Modal.Body>
                         {mensajeError}
+                    </Modal.Body>
+                </Modal>
+                <Modal show={modalBusquedaPrevia} onHide={this.handleModalBusquedasPrevias} size="lg">
+                    <Modal.Header closeButton>
+                    <Modal.Title>{TITULO_BUSQUEDA_PREVIA}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Table striped bordered hover variant="dark">
+                            <thead>
+                                <tr>
+                                    <td>ID Búsqueda</td>
+                                    <td>Usuario que se busco</td>
+                                    <td>Fecha de búsqueda</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {(busquedasPrevias !== null && busquedasPrevias.length === 0) ? null :
+                                    busquedasPrevias.map((busqueda) => {
+                                        if (busqueda === null) {
+                                            return null;
+                                        } 
+                                        return(
+                                            <tr>
+                                                <td>{busqueda.idBusqueda}</td>
+                                                <td>{busqueda.usuarioABuscar}</td>
+                                                <td>{busqueda.fechaBusqueda}</td>
+                                                <td><Button variant="primary">Ver busqueda</Button></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </Table>
                     </Modal.Body>
                 </Modal>
             </div>
---
+++ src/views/components/UserCard.jsx
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import styles from '../../css/UserCard.module.css';
+
+class UserCard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fechaInicio: '',
+        };
+    }
+
+    componentWillMount() {
+        const { startDate } = this.props;
+        let dia = '';
+        let mes = '';
+        const arregloFecha = startDate.split(' ');
+
+        /* 0: "Wed"
+        1: "Sep"
+        2: "14"
+        3: "01:20:18"
+        4: "+0000"
+        5: "2011" */
+
+        switch (arregloFecha[0]) {
+            case 'Sun':
+                dia = 'Domingo';
+                break;
+            case 'Mon':
+                dia = 'Lunes';
+                break;
+            case 'Tue':
+                dia = 'Martes';
+                break;
+            case 'Wed':
+                dia = 'Miércoles';
+                break;
+            case 'Thu':
+                dia = 'Jueves';
+                break;
+            case 'Fri':
+                dia = 'Viernes';
+                break;
+            case 'Sat':
+                dia = 'Sábado';
+                break;
+            default:
+                dia = '';
+        }
+
+        switch (arregloFecha[1]) {
+            case 'Jan':
+                mes = 'Enero';
+                break;
+            case 'Feb':
+                mes = 'Febrero';
+                break;
+            case 'Mar':
+                mes = 'Marzo';
+                break;
+            case 'Apr':
+                mes = 'Abril';
+                break;
+            case 'May':
+                mes = 'Mayo';
+                break;
+            case 'Jun':
+                mes = 'Junio';
+                break;
+            case 'Jul':
+                mes = 'Julio';
+                break;
+            case 'Aug':
+                mes = 'Agosto';
+                break;
+            case 'Sep':
+                mes = 'Septiembre';
+                break;
+            case 'Oct':
+                mes = 'Octubre';
+                break;
+            case 'Nov':
+                mes = 'Noviembre';
+                break;
+            case 'Dec':
+                mes = 'Diciembre';
+                break;
+            default:
+                mes = '';
+        }
+
+        this.setState({ fechaInicio: `${dia} ${arregloFecha[2]} de ${mes}, ${arregloFecha[5]}` });
+    }
+
+    render() {
+        const {
+            name,
+            nickname,
+            location,
+            followers,
+            violento,
+        } = this.props;
+        const { fechaInicio } = this.state;
+        return(
+            <div className={styles.containerCard}>
+                <Col className={styles.columnaDatos1}>
+                    <Row>
+                        <Col>Usuario</Col>
+                        <Col>{name}</Col>
+                    </Row>
+                    <Row>
+                        <Col>Nickname</Col>
+                        <Col>{nickname}</Col>
+                    </Row>
+                    <Row>
+                        <Col>Localización</Col>
+                        <Col>{location}</Col>
+                    </Row>
+                </Col>
+                <Col className={styles.columnaDatos2}>
+                    <Row>
+                        <Col>Seguidores</Col>
+                        <Col>{followers}</Col>
+                    </Row>
+                    <Row>
+                        <Col>Inicio</Col>
+                        <Col>{fechaInicio !== '' ? fechaInicio : 'Sin fecha'}</Col>
+                    </Row>
+                </Col>
+            </div>
+        );
+    }
+}
+
+export default UserCard;
