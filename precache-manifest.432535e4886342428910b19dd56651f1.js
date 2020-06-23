a1ed7a227b23ef49f12575e471c75dd6512b5185
carlostule <carlos.enrique.tule@gmail.com>
2020-06-23 15:54:24 -0500

    Actualización proyecto Tweelock

---
+++ precache-manifest.432535e4886342428910b19dd56651f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd6c939d0d1d489e482c86350144816",
+    "url": "/TweeLock/index.html"
+  },
+  {
+    "revision": "c5a3edc551e5a1e5cda1",
+    "url": "/TweeLock/static/css/2.ba8163e8.chunk.css"
+  },
+  {
+    "revision": "e9e1f7f3adf3c3228f0e",
+    "url": "/TweeLock/static/css/main.cad5c151.chunk.css"
+  },
+  {
+    "revision": "c5a3edc551e5a1e5cda1",
+    "url": "/TweeLock/static/js/2.df0941e7.chunk.js"
+  },
+  {
+    "revision": "e9e1f7f3adf3c3228f0e",
+    "url": "/TweeLock/static/js/main.0103df47.chunk.js"
+  },
+  {
+    "revision": "5ceb49628caec359f9d6",
+    "url": "/TweeLock/static/js/runtime~main.e8c0dfc8.js"
+  },
+  {
+    "revision": "22b2251ca9c2d48daea711b6ba5d9ff7",
+    "url": "/TweeLock/static/media/LogoBotonDesplegado.22b2251c.svg"
+  },
+  {
+    "revision": "7709580a6f0e5947ad57b11cd81f6eaa",
+    "url": "/TweeLock/static/media/Tweelock.7709580a.svg"
+  },
+  {
+    "revision": "7860ee4cdd61ee50f4cd252aeecb2817",
+    "url": "/TweeLock/static/media/logo.7860ee4c.svg"
+  }
+]);
